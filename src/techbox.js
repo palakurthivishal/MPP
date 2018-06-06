173caba764f5efee30600fe168fc313aac83eff9
palakurthivishal <palakurthivishal@gmail.com>
2018-06-06 10:47:17 +0530

    test

--- src/techbox.js
+++ src/techbox.js
@@ -1,17 +1,33 @@
 import React from "react";
 import TechBlock from "./techblock";
+import HighlightTechBlock from "./highlightTech";
 
 const wrapperStyles = {
   // maxHeight: "200px",
   overflow: "auto",
   background: "#EDEDED",
-  padding: "0 10px"
+  padding: "0 10px",
+  position: "relative"
 };
 
-export default ({ data }) => {
-  return (
-    <div class="text-center">
-      <div style={wrapperStyles}>{data.map(d => <TechBlock data={d} />)}</div>
-    </div>
-  );
-};
+export default class TechBox extends React.Component {
+  constructor() {
+    super();
+    this.timerInterval = 300000;
+    this.state = {
+      count: 0
+    };
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {}, this.timerInterval);
+  }
+  startAnimation() {}
+  render() {
+    return (
+      <div style={wrapperStyles} class="text-center">
+        <div>{this.props.data.map(d => <TechBlock data={d} />)}</div>
+        <HighlightTechBlock />
+      </div>
+    );
+  }
+}
